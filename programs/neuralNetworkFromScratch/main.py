4f1a5c179188c525e875aa9fcea9d7e93a29e76f
Zarubin Alexander <zarubin_14@list.ru>
2025-06-20 22:37:07 +0300

    feat(nnfs): softmax function

--- programs/neuralNetworkFromScratch/main.py
+++ programs/neuralNetworkFromScratch/main.py
@@ -1,8 +1,25 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+ 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+# https://gist.github.com/Sentdex/454cb20ec5acf0e76ee8ab8448e6266c
 def spiral_data(points, classes):
     X = np.zeros((points*classes, 2))
     y = np.zeros(points*classes, dtype='uint8')
@@ -16,22 +33,16 @@ def spiral_data(points, classes):
 
 X, y = spiral_data(100, 3)
 
-class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
- 
-class Activation_ReLU:
-    def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
-
-
-layer1 = Layer_Dense(2, 5)
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-# print(layer1.output)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
