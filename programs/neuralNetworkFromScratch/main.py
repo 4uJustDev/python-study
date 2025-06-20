7001fd6a8afc25ef25645d130f94d97a3c842995
Zarubin Alexander <zarubin_14@list.ru>
2025-06-20 22:04:16 +0300

    feat(nnfs): add Relu activation function

--- programs/neuralNetworkFromScratch/main.py
+++ programs/neuralNetworkFromScratch/main.py
@@ -15,28 +15,23 @@ def spiral_data(points, classes):
     return X, y
 
 X, y = spiral_data(100, 3)
-plt.scatter(X[:, 0], X[:, 1])
-plt.show()
 
-plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap='brg')
-plt.show()
-
-# class Layer_Dense:
-#     def __init__(self, n_inputs, n_neurons):
-#         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
-#         self.biases = np.zeros((1, n_neurons))
-#     def forward(self, inputs):
-#         self.output = np.dot(inputs, self.weights) + self.biases
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
  
-# class Activation_ReLU:
-#     def forward(self, inputs):
-#         self.output = np.maximum(0, inputs)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-# print(np.zeros((1, 3)))
 
-# layer1 = Layer_Dense(4, 5)
-# layer2 = Layer_Dense(5, 2)
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
-# layer1.forward(X)
-# layer2.forward(layer1.output)
-# print(layer2.output)
+layer1.forward(X)
+# print(layer1.output)
+activation1.forward(layer1.output)
+print(activation1.output)
